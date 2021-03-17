a3a2be5064b661fbc2bded5789745f389c29bb57
NA-Siddiky <siddiky.academic@gmail.com>
2021-03-17 23:39:43 +0600

    final push for module 43

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
 const Shipment = () => {
-    return (
-        <div>
-            <h1>This is shipment page</h1>
-        </div>
-    );
+  return (
+    <h1>This is shipment from</h1>
+  );
+
+  // const { register, handleSubmit, watch, errors } = useForm();
+  // const onSubmit = data => console.log(data);
+
+  // console.log(watch("example")); // watch input value by passing the name of it
+
+  // return (
+  //   {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+  //   <form onSubmit={handleSubmit(onSubmit)}>
+  //   {/* register your input into the hook by invoking the "register" function */}
+  //     <input name="example" defaultValue="test" ref={register} />
+
+  //     {/* include validation with required or other standard HTML validation rules */}
+  //     <input name="exampleRequired" ref={register({ required: true })} />
+  //     {/* errors will return when field validation fails  */}
+  //     {errors.exampleRequired && <span>This field is required</span>}
+
+  //     <input type="submit" />
+  //   </form>
+  // );
+
+
 };
 
 export default Shipment;
